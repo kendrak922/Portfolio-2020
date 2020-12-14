805825deb9bc2912f4c3a714c3d7a71d084ff92e
Kendra Kyndberg <kendrak922@gmail.com>
2020-12-13 19:50:21 -0600

    mobile compatability and copy

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import volunteerAppPic from '../Pictures/volunteerAppPic.png'
+import getTogetherPic from '../Pictures/getTogether.png'
 import './portfolio.scss'
 
 
@@ -23,15 +24,15 @@ const Portfolio = () => {
                 </div>
                 <div className="item">
                 <div className="image-background">
-                <img src={volunteerAppPic} alt='Volunteer App Screenshot' />
+                <img src={getTogetherPic} alt='Volunteer App Screenshot' />
             </div>
                     <div className="aside">
                         <p className='app-name'>Get Together</p>
                         <p className="description">Get Together is an app for the modern party planner. Its a way to generate meal ideas for a myriad of guest needs - or go rouge and completely ignore everyone.</p>
-                        <p className="description">Technologies implemented include React.js, Sass, Spoonful Recipe API, and </p>
+                        <p className="description">Technologies implemented include React.js, Sass, and the Spoonful Recipe API</p>
                         <div>
                             <a href='https://github.com/kendrak922/Dinner-Party.git' target="_blank" rel="noopener noreferrer" className='button'>Github</a>
-                            <a href='https://volunteer-dev-3ced0.web.app/app.html' target="_blank" rel="noopener noreferrer" className='button'>App</a>
+                            <a href='https://get-together.netlify.app/' target="_blank" rel="noopener noreferrer" className='button'>App</a>
                         </div>
                     </div>
                 </div>
